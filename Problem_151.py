23c13a6da120289c0e1273c990d702330ea53fa4
maximbiryukov <45067231+maximbiryukov@users.noreply.github.com>
2019-09-22 05:16:54 +0300

    Create Problem_151.py

---
+++ Problem_151.py
@@ -0,0 +1,39 @@
+import warnings
+warnings.filterwarnings("ignore")
+
+import random
+import numpy as np
+
+result = []
+
+def job():
+
+    envelope = [16]
+    counter = 0
+
+    while envelope:
+        table = []
+    
+        if len(envelope) == 1 and envelope[0] != 16:
+            counter += 1
+        if envelope[0] == 1 and len(envelope) == 1:
+            break
+            
+        table.append(envelope.pop(random.randint(0,len(envelope)-1)))
+
+        while True:
+            if 1 in table:
+                table.remove(1)
+                envelope += table
+                break
+            else:
+                table += [int(table.pop()/2)]*2
+
+    return counter - 1
+
+for i in range(1000000):
+    if i % 10000 == 0:
+        print(i, np.mean(result))
+    result.append(job())
+    
+print(np.mean(result))
