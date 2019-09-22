fd27056c778c93d5fddfec681d414f8b4101b8b0
maximbiryukov <45067231+maximbiryukov@users.noreply.github.com>
2019-09-22 17:16:08 +0300

    Update Problem_151.py

--- Problem_151.py
+++ Problem_151.py
@@ -1,29 +1,42 @@
-import random
-import numpy as np
-
-result = []
-
-def job():
-
-    envelope = [16]
-    counter = 0
-
-    while envelope:
-        table = []
+def table(sheet):
     
-        if len(envelope) == 1 and envelope[0] != 16:
-            counter += 1
-        if envelope[0] == 1 and len(envelope) == 1:
-            break
+    table = []
+    
+    if sheet != 1:
+        while 1 not in table:
+            sheet /= 2
+            table.append(sheet)
             
-        table.append(envelope.pop(random.randint(0,len(envelope)-1)))
+    return table
 
-        while True:
-            if 1 in table:
-                table.remove(1)
-                envelope += table
-                break
-            else:
-                table += [int(table.pop()/2)]*2
 
-    return counter - 1
+def batch(envelope, options = {():0}):
+              
+    output = 0
+    
+    envelope = tuple(envelope)
+    
+    if envelope and envelope not in options:
+        options[envelope] = 0
+        
+        for item in envelope:
+            
+            new_env = list(envelope)
+            new_env.remove(item)
+            new_env += cut(item)
+            new_env = sorted(new_env)
+            
+            output += batch(tuple(new_env), options)
+            
+        output /= len(envelope)
+        
+        if len(envelope) == 1:
+            output += 1
+            
+        options[envelope] = output
+        
+        
+        
+    return options[envelope]
+
+round(batch((16,))-2, 6)
